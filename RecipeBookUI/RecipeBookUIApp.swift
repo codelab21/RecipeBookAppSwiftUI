4c8ec7667eb7bfe1bdcd0115ed8a38ec2bfb5e01
codelab21 <codelab21@outlook.com>
2023-08-16 13:59:39 +0300

    Initial Commit

---
+++ RecipeBookUI/RecipeBookUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBookUIApp.swift
+//  RecipeBookUI
+//
+//  Created by Eymen on 16.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBookUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
